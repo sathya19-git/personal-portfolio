6198c57801a650362d76c8a5d37450b866283540
Sathya.S <sathya192005@gmail.com>
2026-07-10 17:15:52 +0530

    Add route for Skills component

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+         <Route path="/skills" element={<Skills />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
